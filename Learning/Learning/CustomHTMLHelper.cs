63a663a075df936ead8309f00458f3ccff773011
Aurangajeb Alam <aurangajeb.alam@nesco.gov.bd>
2024-02-28 16:31:46 +0600

    learn html helper and custom html helper

---
+++ Learning/Learning/CustomHTMLHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Learning
+{
+    public static class CustomHTMLHelper
+    {
+        public static IHtmlString Image(string src, string alt)
+        {
+            return new MvcHtmlString(string.Format("<img src='{0}' alt = '{1}'></img>", src, alt));
+        }
+
+        public static IHtmlString Img(this HtmlHelper helper, string src, string alt)
+        {
+            return new MvcHtmlString(string.Format("<img src='{0}' alt = '{1}'></img>", src, alt));
+        }
+    }
+}
