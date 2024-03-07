6a90d96b3c914ce966ad8533cf6b23cd48efe4fd
Aurangajeb Alam <aurangajeb.alam@nesco.gov.bd>
2024-03-07 15:09:06 +0600

    data pass from view to controller

---
+++ Learning/Learning/Controllers/DataPassController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Learning.Controllers
+{
+    public class DataPassController : Controller
+    {
+        // GET: DataPass
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public string FormDataUsingParameter(string firstName, string secondName)
+        {
+            return "Name : " + firstName + " " + secondName;
+        }
+        
+        [HttpPost]
+        public string FormDataUsingRequest()
+        {
+            string firstName = Request["firstName"];
+            string secondName = Request["secondName"];
+            return "Name : " + firstName + " " + secondName;
+        }
+    }
+}
